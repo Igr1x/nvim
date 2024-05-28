948c502980e178f0e85888018a20ee1d6fc82c1f
Igr1x <varnavskii.igrix@mail.ru>
2024-05-28 23:32:00 +0300

    First commit

---
+++ init.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader=" "
+
+vim.opt.number=true
+vim.opt.termguicolors=true
+vim.opt.swapfile=false
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  'morhetz/gruvbox',
+  'ThePrimeagen/vim-be-good',
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' }
+})
+
+require('colorscheme')
+require('lualine').setup()
