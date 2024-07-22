56e85311d5b14dc6276e18d55d536db9d536b317
Igr1x <varnavskii.igrix@mail.ru>
2024-07-22 09:11:48 +0300

    first commit

--- init.lua
+++ init.lua
@@ -1,8 +1,17 @@
 vim.g.mapleader=" "
 
 vim.opt.number=true
+vim.opt.relativenumber=true
 vim.opt.termguicolors=true
 vim.opt.swapfile=false
+vim.opt.clipboard='unnamedplus'
+
+--в режиме поиска убираем выделение
+vim.keymap.set('n', '<esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
+--в visual режиме менеям местами выделенные блок кода
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -19,6 +28,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   'morhetz/gruvbox',
+  'navarasu/onedark.nvim',
   'ThePrimeagen/vim-be-good',
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' }
--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,5 +1,5 @@
 
-local status, _ = pcall(vim.cmd, "colorscheme gruvbox")
+local status, _ = pcall(vim.cmd, "colorscheme onedark")
 if not status then
 	print("Colorscheme not found!")  
 	return
